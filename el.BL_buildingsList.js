3139dde226818f4cdbce20a2f1c29215979d00df
ansceloo <massafra.angelo95@gmail.com>
2022-10-10 19:22:04 +0200

    Start!

---
+++ el.BL_buildingsList.js
@@ -0,0 +1,17 @@
+export const projects = [
+    {
+        name: "Faculty of Engineering",
+        id: "el.BL_ENG",
+        address: "Bologna, Viale Risorgimento, 2",
+        figure: "https://www.storiaememoriadibologna.it/files/vecchio_archivio/seconda-guerra/i/ingegneria.jpg",
+        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/03/"
+    },
+    {
+        name: "Institute of Mathematics",
+        id: "el.BL_MAT",
+        address: "Bologna, Piazza d Porta San Donato, 5",
+        figure: "https://studioleonardo.it/wp-content/uploads/2016/01/011.jpg",
+        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/02/"
+    },
+   
+]
